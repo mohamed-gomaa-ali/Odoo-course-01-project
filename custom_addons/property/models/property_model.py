e498f5db4c2a2bae84bf37a82b8101f9c8724bdc
mohamed gomaa <eng.mohamed.gomaa.ali@gmail.com>
2026-08-12 22:34:01 +0300

    add the views and models files

---
+++ custom_addons/property/models/property_model.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class PropertyModel (models.Model):
+    _name = 'property.model'
+
+    name = fields.Char()
+    description = fields.Char()
+    post_code = fields.Char()
+    date_availabilty = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bed_rooms = fields.Integer()
+    living_area = fields.Integer()
+    garage = fields.Boolean()
+    gardian_area = fields.Integer()
+    gardian_oriantaition = fields.Selection(
+        [
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West")
+        ]
+    )
