371751ec7544cf378dc63e8c2994cfcef168783a
mohamed gomaa <eng.mohamed.gomaa.ali@gmail.com>
2026-08-12 22:33:35 +0300

    add the first files to the project

---
+++ custom_addons/property/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Addons One',
+    'author': 'Mohamed Gomaa Ali',
+    'category': '',
+    'version': '18.0.0.1.0',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/base_menues.xml'
+    ],
+    'application': True,
+}
