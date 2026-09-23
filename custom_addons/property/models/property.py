3ee73181325b28242387e920679dc4f4e4a9ed6d
mohamed gomaa <eng.mohamed.gomaa.ali@gmail.com>
2026-09-23 20:40:12 +0300

    add constarins to name, post_code, bed_rooms, and Living_area to make them required in any make a new record

--- custom_addons/property/models/property.py
+++ custom_addons/property/models/property.py
@@ -1,11 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 # database model for the property app and their columns
 class Property (models.Model):
     _name = 'property'
-    name = fields.Char()
+    name = fields.Char(required = True)
     description = fields.Char()
-    post_code = fields.Char()
+    post_code = fields.Char(required = 1)
     date_availability = fields.Date()
     expected_price = fields.Float()
     selling_price = fields.Float()
@@ -21,3 +21,19 @@ class Property (models.Model):
             ("west", "West")
         ]
     )
+
+    # Decorators
+    # check bedrooms filed if have 0 or not
+    @api.constrains('bed_rooms')
+    def _check_bedrooms_greater_zero(self):
+        for record in self:
+            if record.bed_rooms == 0:
+                raise models.ValidationError("The number of bedrooms must be greater than zero.")
+
+
+    # check living_area filed if have 0 or not
+    @api.constrains('living_area')
+    def _check_living_area_greater_zero(self):
+        for record in self:
+            if record.living_area == 0:
+                raise models.ValidationError("The living area must be greater than zero.")
